9d6da4ca550a74926afa08d92c07641afe34a908
Ubuntu <ubuntu@ip-172-31-35-136.us-east-2.compute.internal>
2019-08-23 23:02:10 +0000

    cleaning

--- run_batch.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-games = ['aliens_variant_1']
-
-for game_name in games:
-	subprocess.call("sbatch rl_runscript.sh {}".format(game_name), shell = True)
--- run_dopamine_openmind_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --qos=tenenbaum
-
-for i in {1..90}
-do
-	sbatch ./run_dopamine_openmind.sh $i
-done
