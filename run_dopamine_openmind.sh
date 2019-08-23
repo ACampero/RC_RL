ad720ddec3872a2f72987cb83f461b6d1f6c0b03
Andres Campero Nunez <campero@openmind7.cm.cluster>
2019-08-23 14:44:07 -0400

    Finished running

--- run_dopamine_openmind.sh
+++ run_dopamine_openmind.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-#SBATH --qos=tenenbaum
 #SBATCH --gres=gpu:1 	        #GPU capabilities
 #SBATCH -n 1                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -t 4-12:00              # Runtime in D-HH:MM
 #SBATCH --mem=35000             # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o ./output/prueba.out      # File to which STDOUT will be written
-#SBATCH -e ./errors/prueba.err      # File to which STDERR will be written
 
 
-declare -A array
-array[1]=aliens
+array=( aliens zelda butterflies plaqueattack expt_antagonist expt_ee
+avoidgeorge jaws watergame boulderdash expt_push_boulders expt_relational
+survivezombies bait expt_preconditions expt_helper chase
+variant_aliens_1 variant_aliens_2 variant_aliens_3 variant_aliens_4
+variant_expt_antagonist_1 variant_expt_antagonist_2 variant_avoidgeorge_1
+variant_avoidgeorge_2 variant_avoidgeorge_3 variant_avoidgeorge_4
+variant_bait_1 variant_bait_2 bees_and_birds variant_bees_and_birds_1
+variant_boulderdash_1 variant_boulderdash_2 variant_butterflies_1
+variant_butterflies_2 variant_chase_1 variant_chase_2 variant_chase_3
+closing_gates variant_closing_gates_1 corridor variant_corridor_1
+variant_expt_ee_1 variant_expt_ee_2 variant_expt_ee_3 variant_frogs_1
+variant_frogs_2 variant_frogs_3 variant_expt_helper_1 variant_expt_helper_2
+variant_jaws_1 variant_jaws_2 lemmings variant_lemmings_1 variant_lemmings_2
+variant_lemmings_3 missilecommand variant_missilecommand_1
+variant_missilecommand_2 variant_missilecommand_3 variant_missilecommand_4
+myAliens variant_myAliens_1 variant_myAliens_2
+variant_plaqueattack_1 variant_plaqueattack_2 variant_plaqueattack_3
+variant_portals_1 variant_portals_2
+variant_expt_preconditions_1 variant_expt_preconditions_2
+variant_expt_push_boulders_1 variant_expt_push_boulders_2
+variant_expt_relational_1 variant_expt_relational_2
+variant_sokoban_1 variant_sokoban_2
+surprise variant_surprise_1 variant_surprise_2
+variant_survivezombies_1 variant_survivezombies_2
+variant_watergame_1 variant_watergame_2
+variant_zelda_1 variant_zelda_2 variant_zelda_3
+frogs portals sokoban)
 
-trial=203
-name=${array[1]}
+name=${array[$1]}
 
 module add openmind/singularity openmind/cudnn openmind/cuda
 cd /om2/user/campero/vgdl_testing/RC_RL/
--- run_dopamine_task.sh
+++ run_dopamine_task.sh
@@ -4,11 +4,11 @@ source venv/bin/activate
 cd dopamine
 
 VGDLNAME="VGDL_$1"
-aux1=gin_bindings='create_atari_environment.game_name="VGDL_$1"'
+aux1=create_atari_environment.game_name=\"VGDL_$1\"
 
 echo $aux1
 
 python -um dopamine.discrete_domains.train \
---base_dir=./tmp/dopamine/$1 \
+--base_dir=./tmp/dopamine/trial1000/$1 \
 --gin_files='dopamine/agents/rainbow/configs/rainbow_aaaiAndres.gin' \
---$aux1
+--gin_bindings=$aux1
